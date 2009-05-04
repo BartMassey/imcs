bee9456a3cbaf7c2396722ac576fc6dec7d19fce
Bart Massey <bart@cs.pdx.edu>
2009-05-04 12:38:38 -0700

    cleaned up a bit

--- Readline.hs
+++ Readline.hs
@@ -1,3 +1,9 @@
+--- Copyright © 2009 Bart Massey
+--- ALL RIGHTS RESERVED
+--- [This program is licensed under the "MIT License"]
+--- Please see the file COPYING in the source
+--- distribution of this software for license terms.
+
 module Readline(hReadline)
 where
 
