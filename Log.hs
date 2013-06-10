ecdeaefd5aeec05f31687b5983b8b4aa67babb47
Bart Massey <bart@cs.pdx.edu>
2013-06-10 15:14:52 -0700

    revamped logging to not fork a separate logging thread
    I have no idea why I originally split off a separate logging
    thread.  I think I must have just been confused.  So weird.

--- Log.hs
+++ Log.hs
@@ -6,7 +6,7 @@
 --- distribution of this software for license terms.
 
 module Log (LogIO, MonadLogIO, liftIO, withLogDo, logMsg, alsoLogMsg,
-           forkLogIO, catchLogIO, whileLogIO, sPutStrLn)
+           forkLogIO, catchLogIO, sPutStrLn)
 where
 
 #if __GLASGOW_HASKELL__ < 706
@@ -16,32 +16,22 @@ import Control.Concurrent
 import Control.Exception.Base
 import Control.Monad.Error
 import Control.Monad.Reader
-import Data.IORef
 import System.IO
 
 import SNewLine
 
-newtype LogMsg = LogMsg String
+type LogIO = ReaderT Handle IO
 
-newtype LogChan = LogChan (Chan LogMsg)
-
-type LogIO = ReaderT LogChan IO
-
-class (MonadIO m, MonadReader LogChan m) => MonadLogIO m
+class (MonadIO m, MonadReader Handle m) => MonadLogIO m
 instance MonadLogIO LogIO
 instance (Error e, MonadLogIO m) => MonadLogIO (ErrorT e m)
 
-run_log :: Handle -> LogChan -> IO ()
-run_log output (LogChan log_chan) =
-    forever $ do
-      LogMsg str <- readChan log_chan
-      hPutStrLn output str
-      hFlush output
-
 logMsg :: MonadLogIO m => String -> m ()
 logMsg msg = do
-  LogChan log_chan <- ask
-  liftIO $ writeChan log_chan (LogMsg msg)
+  h <- ask
+  liftIO $ do
+    hPutStrLn h msg
+    hFlush h
 
 alsoLogMsg :: MonadLogIO m => Handle -> String -> m ()
 alsoLogMsg primary msg = do
@@ -50,30 +40,20 @@ alsoLogMsg primary msg = do
 
 withLogDo :: Handle -> LogIO a -> IO a
 withLogDo h actions = do
-  log_chan <- newChan
-  tid <- forkIO $ run_log h (LogChan log_chan)
-  r <- runReaderT actions (LogChan log_chan)
+  r <- runReaderT actions h
   hClose h
-  killThread tid
   return r
 
 forkLogIO :: LogIO () -> LogIO ThreadId
 forkLogIO actions = do
-  log_chan <- ask
-  liftIO $ forkIO $ runReaderT actions log_chan
+  h <- ask
+  liftIO $ forkIO $ runReaderT actions h
 
 catchLogIO :: LogIO () -> (IOError -> LogIO ()) -> LogIO ()
 catchLogIO actions handler = do
-  log_chan <- ask
-  liftIO $ catch (runReaderT actions log_chan)
-                 (\e -> runReaderT (handler e) log_chan)
-
-whileLogIO :: IORef Bool -> LogIO () -> LogIO ()
-whileLogIO b a = do
-  cond <- liftIO $ readIORef b
-  case cond of
-    False -> return ()
-    True -> a >> whileLogIO b a
+  h <- ask
+  liftIO $ catch (runReaderT actions h)
+                 (\e -> runReaderT (handler e) h)
 
 sPutStrLn :: MonadLogIO m => Handle -> String -> m ()
 sPutStrLn h s = liftIO $ sPutStrNL h s
