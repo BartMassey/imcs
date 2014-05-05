c319d380c6521532f5d9a6894c551a7e8a096349
Bart Massey <bart@cs.pdx.edu>
2014-05-05 13:00:37 +0200

    achieved working cabal file

--- Service.hs
+++ Service.hs
@@ -612,9 +612,10 @@ offerCommand' opt_color opt_times cs = do
               let gl' = game_list'' ++ [ip]
               putState cs $ ServiceState game_id'' gl' pwf''
               let run_game = do
-                  let game_desc = show game_id ++ ": " ++
-                                    p1_name ++ "(" ++ p1_id ++ ", W) vs " ++
-                                    p2_name ++ "(" ++ p2_id ++ ")"
+                  let game_desc =
+                        show game_id ++ ": " ++
+                        p1_name ++ "(" ++ p1_id ++ ", W) vs " ++
+                        p2_name ++ "(" ++ p2_id ++ ")"
                   logMsg $ "game " ++ game_desc ++ " begins"
                   let path = log_path </> show game_id
                   game_log <- liftIO $ openFile path WriteMode
